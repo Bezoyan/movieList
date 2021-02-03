7ddb0f1037c1011192953905d135c53744c0c5c7
Bezoyan <zoya1905bezoyan@gmail.com>
2021-02-03 20:48:28 +0400

    Add movie model and validations

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,10 @@ const startMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
 // const backdrop = document.body.firstElementChild;
 const cancelAddMovieBtn = addMovieMode.querySelector(".btn--passive");
+const addMovieBtn = cancelAddMovieBtn.nextElementSibling;
+const userInput = document.querySelectorAll("input");
+
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -17,6 +21,31 @@ const backdropClickHandler = () => {
   toggleMovieModel();
 };
 
+const addMovie = () => {
+  const titleValue = userInput[0].value;
+  const imageURLValue = userInput[1].value;
+  const raitingValue = userInput[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageURLValue.trim() === "" ||
+    raitingValue.trim() === "" ||
+    +raitingValue < 1 ||
+    +raitingValue > 5
+  ) {
+    alert("Pleas enter valid value");
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageURLValue,
+    raiting: raitingValue,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+};
+
 const cancelAddMovie = () => {
   toggleMovieModel();
 };
@@ -24,3 +53,4 @@ const cancelAddMovie = () => {
 startMovieButton.addEventListener("click", toggleMovieModel);
 backdrop.addEventListener("click", backdropClickHandler);
 cancelAddMovieBtn.addEventListener("click", cancelAddMovie);
+addMovieBtn.addEventListener("click", addMovie);
